116b9d9980e23362451a0d8aded2a41b3b973161
PhredE <phrede@PhredEs-MacBook-Air.local>
2022-06-09 10:41:18 -0700

    initial route functioning

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
+import bodyParser from 'body-parser'
 
+import posts from './routes/posts.js';
 
 const app = express();
+
+app.use(bodyParser, json());
+
+app.use('/api', posts);
 
 app.get('/', (req, res) => {
     res.json ({response: "hello world"
